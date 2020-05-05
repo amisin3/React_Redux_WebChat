cefb2ec0ea1df82bd8726b61b9debdc68ae02d8a
amisin3 <amit1291997@gmail.com>
2020-05-05 14:54:14 +0530

    Taking care of security

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import "firebase/storage";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDL9oOIxaJ6XqAjvvY80N_16aHCX6AORMU",
-  authDomain: "my-react-slack-clone-1376c.firebaseapp.com",
-  databaseURL: "https://my-react-slack-clone-1376c.firebaseio.com",
-  projectId: "my-react-slack-clone-1376c",
-  storageBucket: "my-react-slack-clone-1376c.appspot.com",
-  messagingSenderId: "361528575684",
-  appId: "1:361528575684:web:33d12a75553f961fdcab91",
-  measurementId: "G-JMTSTV6D8R"
+  apiKey: "", // Your Api key
+  authDomain: "", // Your Auth Domain
+  databaseUrl: "", // Your Database Url
+  projectId: "", // Your project id
+  storageBucket: "", // Your storage bucket
+  messagingSenderId: "", // Your sender id
+  appId: "", // Your app id
+  measurementId: "", // Your measurement id
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
